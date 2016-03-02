baa98c58638b65d1615d71a5ea76ec6ebf2ea4c5
emilien <emilien.mottet@etu.udamail.fr>
2016-03-02 09:02:36 +0100

    test

---
+++ powerchat/app/src/main/java/pic/pipic1/powerchat/Controler/ControlerMain.java
@@ -0,0 +1,7 @@
+package pic.pipic1.powerchat.Controler;
+
+/**
+ * Created by emimo on 02/03/2016.
+ */
+public class ControlerMain {
+}
---
+++ powerchat/app/src/main/java/pic/pipic1/powerchat/Model/User.java
@@ -0,0 +1,7 @@
+package pic.pipic1.powerchat.Model;
+
+/**
+ * Created by emimo on 02/03/2016.
+ */
+public class User {
+}
