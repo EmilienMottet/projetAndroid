fad0e18555db4048dd0d19bb8845a1daa22f6732
pipic1 <pierre.pic63122@gmail.com>
2016-03-08 23:45:33 +0100

    Correction du layout ==> fonctionnel

--- powerchat/app/src/main/java/pic/pipic1/powerchat/View/Discussion/DiscussionActivity.java
+++ powerchat/app/src/main/java/pic/pipic1/powerchat/View/Discussion/DiscussionActivity.java
@@ -10,12 +10,14 @@ import android.view.View;
 import pic.pipic1.powerchat.R;
 
 public class DiscussionActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discussion);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("The subject");
         setSupportActionBar(toolbar);
     }
 
